9eb56f0c53c13b3813d98ad22222b87c50334a15
yulya-buraya <buraya.yulya@gmail.com>
2020-09-05 17:17:22 +0300

    Initial commit

---
+++ app/src/main/java/by/bestu/fit/buraya/firstapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package by.bestu.fit.buraya.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//TODO Add new method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
